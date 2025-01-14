8a8fe5f517bc3a7c4353a75eaeadfface81fd646
Alexi Saravia <danviaz@gmail.com>
2025-01-14 14:15:15 -0600

    Update url cv in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                     <motion.h2 variants={childVariants} className="pb-2 text-4xl tracking-tighter lg:text-8xl">Daniel Saravia</motion.h2>
                     <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Software Engineer || Python - Django || React - Node.js </motion.span>
                     <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">{HERO_CONTENT}</motion.p>
-                    <motion.a variants={childVariants} href="/AlexiSaravia-SoftwareEngineer-CV.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume</motion.a>
+                    <motion.a variants={childVariants} href="https://github.com/iodaniels/portfolio/blob/master/public/AlexiSaravia-SoftwareEngineer-CV.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume</motion.a>
                 </motion.div>
             </div>
         </div>
