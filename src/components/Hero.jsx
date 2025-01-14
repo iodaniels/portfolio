c8c464ab125ce792766d6ab03276aa55f3bd61aa
Alexi Saravia <danviaz@gmail.com>
2025-01-14 13:38:19 -0600

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import { HERO_CONTENT } from '../constants';
+import { motion } from 'framer-motion'
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    }
+  }
+}
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } }
+}
+
+const Hero = () => {
+  return (
+    <div className="pb-4 lg:mb-36">
+        <div className="flex flex-wrap items-center justify-center">
+            <div>
+                <motion.div className="flex flex-col items-center lg:items-start mt-10" initial="hidden" animate="visible" variants={containerVariants}>
+                    <motion.h2 variants={childVariants} className="pb-2 text-4xl tracking-tighter lg:text-8xl">Daniel Saravia</motion.h2>
+                    <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Software Engineer || Python - Django || React - Node.js </motion.span>
+                    <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">{HERO_CONTENT}</motion.p>
+                    <motion.a variants={childVariants} href="/AlexiSaravia-SoftwareEngineer-CV.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume</motion.a>
+                </motion.div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+
+export const HERO_CONTENT = `More than 7 years of experience as a Software Engineer. Developing digital products for the health, logistics and retail industry, with +10 thousand users on a recurring basis monthly, generating +50 thousand transactions monthly between all the digital products developed. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Python, Django, Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const EXPERIENCES = [
+  {
+    year: "March 2022 - November 2024",
+    role: "Mid-Senior Software Engineer ",
+    company: "Multisoft Honduras",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, Python and Django. Implemented RESTful APIs and integrated with Postgresql databases. Collaborated with stakeholders to define project requirements and timelines. Developing and maintaining web platform for business sales management in the health industry, managing +3 thousand work orders monthly, optimizing the critical processes involved in the business sales division, increasing sales by 10% monthly. Developing and maintaining web application to make quotes for clinical studies, processing +5 thousand quotes monthly.`,
+    technologies: ["Python", "React.js", "Next.js", "Django", "Postgresql"],
+  },
+  {
+    year: "October 2020 - March 2022",
+    role: "Full Stack Engineer",
+    company: "Multisoft Honduras",
+    description: `Developed and maintained web applications using JavaScript, React.js, Node.js, Python and Django. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Javascript", "Node.js", "React.js", "MySQL", "Python", "Django", "MongoDB"],
+  },
+  {
+    year: "May 2018 - October 2020",
+    role: "Junior Software Engineer",
+    company: "Multisoft Honduras",
+    description: `Contributed to the development of web applications using JavaScript, React, Python and Django. Managed databases and implemented data storage solutions using MySQL and MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["React.js", "Node.js", "Python", "Django", "MySQL", "MongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Hospital Management System",
+    description:
+      "A hospital management system is a software designed to assist in the management and administration of the different processes and resources within a hospital or healthcare institution. This system integrates various functionalities that allow an efficient control of medical, administrative, financial and operational services, improving the quality of service and reducing operating costs.",
+    technologies: ["Python", "Django", "Bootstrap", "MySQL"],
+    github: "https://github.com/iodaniels/health-management-system"
+  },
+  {
+    title: "Learning Management System",
+    description:
+      "A technological platform designed to offer distance education through the use of the Internet and associated technologies. This type of system allows students, teachers and administrators to interact in a virtual environment, facilitating teaching and learning in a flexible, accessible and effective manner. E-learning systems are used by educational institutions, companies and organizations for continuing education, professional training, and distance learning academic programs.",
+    technologies: ["Python", "Django Rest Framework", "React.js", "Postgresql"],
+    github: "https://github.com/iodaniels/learning-management-system"
+  },
+];
+
+export const CONTACT = {
+  address: "Armenco Road Zip Lane, New Iberia, LA 70560",
+  email: "danviaz@gmail.com",
+};
